4b523f31bbb3b45a12b37d4064e9c7a52587b9c1
matteocuellarvega <matteocuellar@me.com>
2025-06-23 13:39:52 +0100

    Updating booking templates.

--- src/Services/PdfService.php
+++ src/Services/PdfService.php
@@ -8,9 +8,11 @@ class PdfService
 {
     public static function generateReceipt(string $html, string $filePath): void
     {
+        print_r("Generating PDF receipt...\n");
+        print_r("Directory: " . dirname(__DIR__, 2) . "\n");
         $options = new Options();
         $options->set('isRemoteEnabled', true);
-        $options->set('chroot', dirname(__DIR__, 2));
+        $options->set('chroot', dirname(__DIR__, 2) . '/public'); // Set the chroot to the public directory
         $options->set('defaultMediaType', 'print'); 
 
         $dompdf = new Dompdf($options);
