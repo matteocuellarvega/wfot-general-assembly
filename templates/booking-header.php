71bf0a4609dfecffd3432234e74e5018503e3013
matteocuellarvega <matteocuellar@me.com>
2025-06-23 11:05:31 +0100

    Updating booking templates.

--- templates/booking-header.php
+++ templates/booking-header.php
@@ -3,6 +3,9 @@
 <html lang="en"><head>
 <meta charset="utf-8">
 <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@2/css/pico.min.css">
+<link rel="stylesheet" href="https://<?php echo $_SERVER['HTTP_HOST']; ?>/assets/css/global.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <title>WFOT Booking</title>
 </head><body class="container">
 <img src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/assets/img/logo-<?php echo strtolower($meetingId); ?>.svg" alt="WFOT Logo" class="logo">
